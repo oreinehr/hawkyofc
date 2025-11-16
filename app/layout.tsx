b656c361dee6d65bcc8b3df487a81c356d06797c
oreinehr <guireinehr02@gmail.com>
2025-11-16 15:56:24 -0300

    dsgjsda

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Hawku',
+  title: 'Hawky',
   description: "",
   generator: '',
 }
